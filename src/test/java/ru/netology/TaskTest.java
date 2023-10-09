0df23cb488d91ce42baa7fc8d1a5bf1c164033c6
Nastura <chulmost@gmail.com>
2023-10-09 16:04:20 +0300

    first version dz

---
+++ src/test/java/ru/netology/TaskTest.java
@@ -0,0 +1,51 @@
+package ru.netology;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+
+class TaskTest {
+
+    @Test
+    public void testTrueOrFalseSimpleTask() {
+        SimpleTask simpleTask = new SimpleTask(5, "Написать маме");
+
+        Assertions.assertTrue(simpleTask.matches("Написать")); // проверка существующейго слова
+        Assertions.assertTrue(simpleTask.matches("Написать маме")); // проверка существующего словосочетания
+        Assertions.assertFalse(simpleTask.matches("написать")); // проверка регистра
+        Assertions.assertFalse(simpleTask.matches("начальнику")); //проверка несуществующей строки
+    }
+
+    @Test
+    public void testTrueOrFalseMeeting() {
+        Meeting meeting = new Meeting(
+                555,
+                "Выкатка 3й версии приложения",
+                "Приложение НетоБанка",
+                "Во вторник после обеда");
+
+        Assertions.assertTrue(meeting.matches("Выкатка")); // проверка поиска в topic
+        Assertions.assertFalse(meeting.matches("выкатка")); // проверка поиска в topic регистр
+        Assertions.assertFalse(meeting.matches("Выкатку")); // проверка поиска в topic окончание
+        Assertions.assertFalse(meeting.matches("Обновление")); // проверка поиска отсутствующего слова
+        Assertions.assertTrue(meeting.matches("НетоБанка")); // проверка поиска в project
+        Assertions.assertTrue(meeting.matches("НетоБанк")); // проверка поиска в project без окончания
+        Assertions.assertFalse(meeting.matches("Нетобанка")); // проверка поиска в project регистр
+        Assertions.assertFalse(meeting.matches("вторник")); // проверка поиска в start
+    }
+    @Test
+    public void testTrueOrFalseEpic() {
+        String[] subtasks = { "Молоко", "Яйца", "Хлеб" };
+        Epic epic = new Epic(55, subtasks);
+
+        Epic epicEmpty = new Epic(55, new String[]{});
+
+        Assertions.assertTrue(epic.matches("Молоко")); // проверка поиска
+        Assertions.assertFalse(epic.matches("молоко")); // проверка поиска регистр
+        Assertions.assertFalse(epic.matches("Яица")); // проверка поиска й/и
+        Assertions.assertFalse(epic.matches("Хлеба")); // проверка поиска окончание
+        Assertions.assertFalse(epic.matches("Банан")); // проверка поиска отсутствующего слова
+        Assertions.assertFalse(epicEmpty.matches("Молоко")); // проверка пустого массива
+    }
+}
+//Assertions.assertTrue(simpleTask.matches(" ")); // проверка пробел
---
+++ src/test/java/ru/netology/TodosTest.java
@@ -0,0 +1,66 @@
+package ru.netology;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+class TodosTest {
+    SimpleTask simpleTask = new SimpleTask(5, "Написать маме");
+
+    String[] subtasks = {"Молоко", "Яйца", "Хлеб"};
+    Epic epic = new Epic(55, subtasks);
+
+    Meeting meeting = new Meeting(
+            555,
+            "Выкатка 3й версии приложения",
+            "Приложение НетоБанка установить родителям",
+            "Во вторник после обеда"
+    );
+
+    Todos todos = new Todos();
+
+    @BeforeEach
+    public void setup() {
+        todos.add(simpleTask);
+        todos.add(epic);
+        todos.add(meeting);
+    }
+
+    @Test
+    public void shouldAddThreeTasksOfDifferentType() {
+
+
+        Task[] expected = {simpleTask, epic, meeting};
+        Task[] actual = todos.findAll();
+        Assertions.assertArrayEquals(expected, actual);
+    }
+
+    @Test
+    public void shouldSearchQuery1TaskEpic() {
+        Task[] expected = {epic};
+        Task[] actual = todos.search("Молоко"); // проверка epic (находится 1 задача)
+        Assertions.assertArrayEquals(expected, actual);
+    }
+
+    @Test
+    public void shouldNotSearchQueryStart() {
+        Task[] expected = {};
+        Task[] actual = todos.search("обеда"); // проверка meeting start (запрос не должен быть найден)
+        Assertions.assertArrayEquals(expected, actual);
+    }
+
+    @Test
+    public void shouldSearchQueryIfNotExist() {
+        Task[] expected = {};
+        Task[] actual = todos.search("мыло"); // проверка когда не находится ни одна задача
+        Assertions.assertArrayEquals(expected, actual);
+    }
+
+    @Test
+    public void shouldSearchQueryIfFSomeResults() {
+        Task[] expected = {simpleTask, meeting};
+        Task[] actual = todos.search("родителям"); // проверка нескольких результатов
+        Assertions.assertArrayEquals(expected, actual);
+    }
+    
+}
